dc466228915818bb03a44f46b7569f33deaa6273
Chinmay <surtichinmay@yahoo.in>
2017-11-11 12:26:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -292,5 +292,5 @@ app.listen(port, () => {
 
 // pings server every 15 minutes to prevent dynos from sleeping
  setInterval(() => {
-  http.get('https://ainsley-bot.herokuapp.com/');
+  http.get('http://ainsley-bot.herokuapp.com/');
  }, 900000);
